e53154ab00bd5a7f466a369acd96d897accbf10b
kaushalkotian19-sketch <kaushalkotian19@gmail.com>
2026-05-06 18:01:25 +0530

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,1 +1,12 @@
+// database.js - Persistent Local Saving
 
+function getTotalCoins() {
+    return parseInt(localStorage.getItem('hillClimbBank')) || 0;
+}
+
+function saveCoins(newCoins) {
+    let currentBank = getTotalCoins();
+    currentBank += newCoins;
+    localStorage.setItem('hillClimbBank', currentBank);
+    return currentBank;
+}
