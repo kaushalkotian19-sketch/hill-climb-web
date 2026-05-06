112145ed65e3d4d0f6a77e26f6a1fa9fb74de51d
kaushalkotian19-sketch <kaushalkotian19@gmail.com>
2026-05-06 22:47:57 +0530

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,5 +1,6 @@
-// database.js - Persistent Local Saving
+// database.js - Persistent Local Saving & Economy
 
+// --- COIN BANK ---
 function getTotalCoins() {
     return parseInt(localStorage.getItem('hillClimbBank')) || 0;
 }
@@ -9,4 +10,24 @@ function saveCoins(newCoins) {
     currentBank += newCoins;
     localStorage.setItem('hillClimbBank', currentBank);
     return currentBank;
+}
+
+function spendCoins(amount) {
+    let currentBank = getTotalCoins();
+    if (currentBank >= amount) {
+        currentBank -= amount;
+        localStorage.setItem('hillClimbBank', currentBank);
+        return true; 
+    }
+    return false; 
+}
+
+// --- VEHICLE UNLOCKS ---
+function isVehicleUnlocked(vehicleType) {
+    if (vehicleType === 'jeep') return true; // Starter car
+    return localStorage.getItem('unlocked_' + vehicleType) === 'true';
+}
+
+function unlockVehicle(vehicleType) {
+    localStorage.setItem('unlocked_' + vehicleType, 'true');
 }
